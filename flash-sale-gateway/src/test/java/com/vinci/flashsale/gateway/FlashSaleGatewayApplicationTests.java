9404dd5235caf96ffa4b75d6a771cf64b81a34fc
vinci <staramau233@outlook.com>
2025-12-16 18:20:39 +0800

    加 API Documentation Style Guide API 文档编写风格指南 - 补充 Code Review Checklist 代码审查清单提升评审效率效果 - 增加 Pullfeat(gateway): 初始化秒杀网关应用 Request Template PR
    - 添加秒杀网关主

---
+++ flash-sale-gateway/src/test/java/com/vinci/flashsale/gateway/FlashSaleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinci.flashsale.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
